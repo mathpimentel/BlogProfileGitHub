1bf5cae8dbe0224169c790c55aeeeb608659faa7
Matheus Pimentel <mathcamposp@gmai.com>
2025-03-23 23:44:38 -0300

    Adicionando token de autorização no axios e chamando o component Issues dentro do Summary/index.tsx

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Issues } from "../Issues";
 
 export async function getGitHubUser(username: string) {
   const userApi = axios.create({
@@ -9,6 +10,7 @@ export async function getGitHubUser(username: string) {
     timeout: 5000, // Tempo máximo para a requisição
     headers: {
       "Content-Type": "application/json", // Tipo de conteúdo para a requisição
+      Authorization: `token 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`,
     },
   });
 
@@ -39,10 +41,13 @@ export function Summary() {
   const [userData, setUserData] = useState<any>(null); // Estado que armazena os dados da API
   const [loading, setLoading] = useState<boolean>(true); // Estado que controla o carregamento
 
+  const username = "lucaspedronet";
+  const repositorio = "BlogProfileGitHub";
+
   useEffect(() => {
-    const username = "mathpimentel";
     getGitHubUser(username).then((data) => {
       if (data) {
+        console.log(data);
         setUserData(data); // Atualiza o estado dos dados do usuário
       }
       setLoading(false); // Finaliza o carregamento
@@ -58,36 +63,43 @@ export function Summary() {
   }
 
   return (
-    <SummaryContainer>
-      <img src={userData.avatar} alt="Avatar do GitHub" />
-      <section>
-        <SummaryHeader>
-          <h1>{userData.nome}</h1>
-          <a href={`https://github.com/${userData.login}`} target="_blank">
-            GITHUB
-            <ArrowUpRight size={12} />
-          </a>
-        </SummaryHeader>
+    <>
+      <SummaryContainer>
+        <img src={userData.avatar} alt="Avatar do GitHub" />
+        <section>
+          <SummaryHeader>
+            <h1>{userData.nome}</h1>
+            <a href={`https://github.com/${userData.login}`} target="_blank">
+              GITHUB
+              <ArrowUpRight size={12} />
+            </a>
+          </SummaryHeader>
 
-        <p>{userData.bio}</p>
+          <p>{userData.bio}</p>
 
-        <SummaryAnchors>
-          <div>
-            <GithubLogo size={18} />
-            <span>{userData.login}</span>
-          </div>
+          <SummaryAnchors>
+            <div>
+              <GithubLogo size={18} />
+              <span>{userData.login}</span>
+            </div>
 
-          <div>
-            <Buildings size={18} />
-            <span>{userData.profissao}</span>
-          </div>
+            <div>
+              <Buildings size={18} />
+              <span>{userData.profissao}</span>
+            </div>
 
-          <div>
-            <Users size={18} />
-            <span>{userData.seguidores} seguidores</span>
-          </div>
-        </SummaryAnchors>
-      </section>
-    </SummaryContainer>
+            <div>
+              <Users size={18} />
+              <span>{userData.seguidores} seguidores</span>
+            </div>
+          </SummaryAnchors>
+        </section>
+      </SummaryContainer>
+
+      <div>
+        <h2 style={{ padding: "1rem 0" }}>Issues do projeto {repositorio}</h2>
+        <Issues username={userData.login} repo={repositorio} />
+      </div>
+    </>
   );
 }
