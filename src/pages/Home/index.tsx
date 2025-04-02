ca86199889c59c309e2f50854264204f12fc804b
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-02 19:46:32 -0300

    configurando o component Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,25 @@
-
+import { useEffect, useState } from "react";
+import { Issues } from "./components/Issues";
 import { Summary } from "./components/Summary";
-import { HomeContainer } from "./styles";
+import { HomeContainer, IssuesAside } from "./styles";
+import { api } from "../../lib/api";
+
+interface Issue {
+  number: number;
+  title: string;
+  body: string;
+  create_at: string;
+}
 
 export function Home() {
 
   return (
     <HomeContainer>
       <Summary />
+
+      <IssuesAside>
+        <Issues />
+      </IssuesAside>
     </HomeContainer>
   );
 }
