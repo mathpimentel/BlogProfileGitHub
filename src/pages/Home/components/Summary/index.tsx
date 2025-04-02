c719340dd65f8c133f88fa6a4cd334e879c36817
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-02 19:28:29 -0300

    correção do professor

--- src/pages/Home/components/Summary/index.tsx
+++ src/pages/Home/components/Summary/index.tsx
@@ -1,9 +1,29 @@
 import { SummaryAnchors, SummaryContainer, SummaryHeader } from "./styles";
-
 import { ArrowUpRight, Buildings, GithubLogo, Users } from "phosphor-react";
+import { useEffect, useState } from "react";
+import { api } from "../../../../lib/api";
 
+interface User {
+  avatar_url: string;
+  bio: string;
+  company: string;
+  followers: number;
+  login: string;
+  name: string;
+}
 
+// 4148bec073bade264375123827ee83dd6812221c
 export function Summary() {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function fetchUser() {
+    const response = await api.get<User>("/users/lucaspedronet");
+    setUser(response.data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   // "https://api.github.com/users", "/lucaspedronet"
   // "https://api.github.com/search"
@@ -11,30 +31,30 @@ export function Summary() {
 
   return (
     <SummaryContainer>
-      <img src="../src/assets/avatar.JPG" />
+      <img src={user?.avatar_url} />
       <section>
         <SummaryHeader>
-          <h1>Lucas Pedro</h1>
-          <a href="#" target="_blank">
+          <h1>{user?.name}</h1>
+          <a href="" target="_blank">
             GITHUB
             <ArrowUpRight size={12} />
           </a>
         </SummaryHeader>
-        <p>Software Engineering. developer at NodeJS, ReactJS, React Native, Electron.</p>
+        <p>{user?.bio}</p>
         <SummaryAnchors>
           <div>
             <GithubLogo size={18} />
-            <span>lucaspedronet</span>
+            <span>{user?.login}</span>
           </div>
 
           <div>
             <Buildings size={18} />
-            <span>Paraná Banco</span>
+            <span>{user?.company}</span>
           </div>
 
           <div>
             <Users size={18} />
-            <span>47</span>
+            <span>{user?.followers} seguidores</span>
           </div>
         </SummaryAnchors>
       </section>
